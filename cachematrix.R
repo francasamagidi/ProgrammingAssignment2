c842271b2420fbc7e77a8c3cc47ccade73525a2b
francasamagidi <preciousfranca@yahoo.com>
2015-10-25 15:01:30 +0100

    Update cache matrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,69 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## The inverse of a matrix can be cache in very long vectors 
+## where the contents of a vector
+## has not changed, in order for the mean of the matrix 
+## to be looked up in the cache when needed instead of computing the mean repeatedly. 
+## This function creates a special "matrix" MakeVector that cache it's inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
 
+MakeVector <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set,
+       get = get,
+       setmean = setmean,
+       getmean= getmean)
 }
 
 
-## Write a short comment describing this function
+## This function calculates the inverse of the  "MakeVector"
+## and also retrieve the inverse 
+## from the cache in cases where the matrix has not changed and the inverse has been computed, 
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cachemean <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getmean()
+  if (!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  mat <- x$get()
+  m <- solve(mat, ...)
+  x$setmean(m)
+  m
 }
+
+
+cachematrix <- MakeVector(matrix(7:10, 2, 2))
+
+cachematrix$get()
+
+cachematrix$getmean()
+
+cachemean(cachematrix)
+
+cachemean(cachematrix)
+
+
+cachematrix$getmean()
+
+cachematrix$set(matrix(c(6, 8, 3, 2), 2, 2))
+
+cachematrix$get()
+
+cachematrix$getmean()
+
+cachemean(cachematrix)
+
+cachemean(cachematrix)
+
+cachematrix$getmean()
+
+
+
